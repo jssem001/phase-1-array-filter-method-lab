fb2b45678398dbcc9229ccbf1144646053f7c520
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-25 22:08:36 +0300

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(arr, name) {
+    return arr.filter((el) => el.toLowerCase().includes(name.toLowerCase()));
+}
+
+function fuzzyMatch(arr, letters) {
+    letters = letters.toLowerCase()
+    let matched = arr.filter(name => {
+        const lowerCaseName = name.toLowerCase();
+        return lowerCaseName.startsWith(letters);
+             })
+    return matched
+}
+
+function matchName(arr, namev){
+    return arr.filter((record) => record.name === namev);
+}
